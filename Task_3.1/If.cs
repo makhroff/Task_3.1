dd0735eefd3cac4449ef1e9ac5a1ab6c168b8ff9
Philippe - A <91776022+makhroff@users.noreply.github.com>
2022-09-10 11:12:14 +0200

    if 15 fix

--- Task_3.1/If.cs
+++ Task_3.1/If.cs
@@ -36,8 +36,8 @@
         }
         public static int Task_15(int num1, int num2, int num3)
         {
-            if (num1 > num3 && num2 > num3) return num1 + num2;
-            if (num2 > num1 && num3 > num1) return num2 + num3;
+            if (num1 >= num3 && num2 >= num3) return num1 + num2;
+            if (num2 >= num1 && num3 >= num1) return num2 + num3;
             return num3 + num1;
         }
         public static int Task_18(int num1, int num2, int num3)
